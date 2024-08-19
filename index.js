f545bfe6901e77511d7fa361d63b71dd4ed184bb
Victor <vsingh57@myseneca.ca>
2024-08-19 16:57:59 -0400

    Added Cors, Account API

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require("express");
+const cors = require("cors"); // Import the cors package
 const app = express();
 const port = 3001;
 const AccountsRouter = require('./apis/Accounts');
+
+// Configure CORS
+const allowedOrigins = ['http://localhost:3002', 'https://victorsingh.ca'];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = `The CORS policy for this site does not allow access from the specified origin: ${origin}`;
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+    optionsSuccessStatus: 200 // Some legacy browsers choke on a 204 status
+}));
 
 app.use(express.json());
 
